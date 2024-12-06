f96e4ebeee84999b2bcdb412b966442bd9a59cc2
RaulErnesto08 <ernesto.villarreal@encora.com>
2024-12-06 11:12:41 -0600

    Refactor utils module

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,4 +1,11 @@
-from .video_utils import extract_audio
+from .video_utils import extract_audio, extract_keyframes
 from .transcription_utils import transcribe_audio
+from .scene_utils import extract_keywords_with_gpt, describe_scene
+from .summarize_utils import generate_summary_with_gpt
 
-__all__ = ["extract_audio", "transcribe_audio"]
+__all__ = [
+            "extract_audio", "extract_keyframes", 
+            "transcribe_audio", 
+            "extract_keywords_with_gpt", "describe_scene",
+            "generate_summary_with_gpt"
+           ]
